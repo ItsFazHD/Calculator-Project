93d1d89d67cf69e127451ceb29a7cb1bcb3f0ee2
Shah Faisal Alam <fazhd@hotmail.com>
2023-11-02 01:21:10 +0000

    Completed Division class also made the text more concise

--- src/Division.java
+++ src/Division.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Division {
     public static void division() {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Division: \nPlease enter a number: ");
+        double first = input.nextDouble();
+
+        System.out.println("Please enter another number: ");
+        double second = input.nextDouble();
+
+        double sum = first / second;
+        System.out.println("The total is: " + sum);
+
+        System.out.println("Continue? \n1 Return to menu \n2 Continue division \n0 Exit");
+
+        switch (input.nextInt()) {
+            case 1:
+                Main.main(null);
+                break;
+            case 2:
+                Division.division();
+                break;
+            case 0:
+                input.close();
+                break;
+        }
     }
 }
